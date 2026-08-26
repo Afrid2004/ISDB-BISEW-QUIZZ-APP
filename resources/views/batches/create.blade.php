0f235e23813b8b24a6188c94cda46eb9eb8205e4
afrid2004 <mdfaisalafrid@gmail.com>
2026-08-26 16:30:09 +0600

    added batches

--- resources/views/batches/create.blade.php
+++ resources/views/batches/create.blade.php
@@ -12,9 +12,6 @@
         <div class="mb-6 flex flex-col gap-4 sm:flex-row sm:items-center sm:justify-between">
 
             <div class="flex items-center gap-3">
-                <div class="flex h-10 w-10 items-center justify-center rounded-lg bg-primary/10 text-primary">
-                    <i class="bi bi-people-fill text-lg"></i>
-                </div>
 
                 <div>
                     <h1 class="text-2xl font-bold text-slate-800">
--- resources/views/batches/edit.blade.php
+++ resources/views/batches/edit.blade.php
@@ -13,10 +13,6 @@
 
             <div class="flex items-center gap-3">
 
-                <div class="flex h-10 w-10 items-center justify-center rounded-lg bg-primary/10 text-primary">
-                    <i class="bi bi-people-fill text-lg"></i>
-                </div>
-
                 <div>
                     <h1 class="text-2xl font-bold text-slate-800">
                         Edit Batch
